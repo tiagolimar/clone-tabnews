0f239a7c1e07a8e7eeb6de5dabb948e9f65996ac
Tiago Lima <47084105+tiagolimar@users.noreply.github.com>
2025-08-20 15:15:54 +0000

    adicionando arquivos

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    const style={
+        fontFamily: 'Calibri',
+        fontSize: '54px',
+        textAlign: 'center'
+    }
+    return <h1 style={style}>Teste</h1>;
+}
