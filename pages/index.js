917b685175c81489670f469f4f0da1e27b4acac9
Tiago Lima <47084105+tiagolimar@users.noreply.github.com>
2025-08-28 15:18:07 +0000

    atualiza frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,4 @@
 export default function Home() {
-<<<<<<< HEAD
-    const style = {
-        fontFamily: "Calibri",
-        fontSize: "54px",
-        textAlign: "center",
-    };
-    return <h1 style={style}>Teste</h1>;
-}
-=======
     const style={
         fontFamily: 'Calibri',
         fontSize: '54px',
@@ -15,4 +6,3 @@ export default function Home() {
     }
     return <h1 style={style}>Seja Bem Vindo!</h1>;
 }
->>>>>>> 793fbd3c8ad7f4f49cd9330364d743abfdf78947
