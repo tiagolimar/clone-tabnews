5100337d92f4421b6e48b2f8dea408ff801073c4
Tiago Lima <47084105+tiagolimar@users.noreply.github.com>
2025-09-13 18:25:29 +0000

    adiciona os arquivo `database.js` e `.env`

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,7 @@
+import database from "../../../../../infra/database.js";
+
 test("GET to /api/v1/status should return status 200", async () => {
+  console.log((await database.query("SELECT 1+1 as sum;")).rows);
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
---
+++ test/integration/api/v1/status/tentativa.js
@@ -0,0 +1,3 @@
+module.exports = function tentativa() {
+  return null;
+};
